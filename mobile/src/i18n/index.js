a372c384228428bd91d2d8db4ece1df4bf0014eb
Adam Liechty <adam@doubledutch.me>
2019-02-28 16:08:27 -0800

    translations for arabic, german, spanish, french, italian, japanese, dutch

--- mobile/src/i18n/index.js
+++ mobile/src/i18n/index.js
@@ -1,11 +1,30 @@
-import enUS from './en-us/surveys.mobile.json'
-// import deDE from './de-de/surveys.mobile.json'
+import enUS from './en-us/surveys.admin.json'
 
-export default (strings = {
+import arINT from './ar-int/surveys.admin.json'
+import deDE from './de-de/surveys.admin.json'
+import esES from './es-es/surveys.admin.json'
+import frFR from './fr-fr/surveys.admin.json'
+import itIT from './it-it/surveys.admin.json'
+import jaJP from './ja-jp/surveys.admin.json'
+import nlNL from './nl-nl/surveys.admin.json'
+
+export default {
   '': enUS,
   en: enUS,
   'en-US': enUS,
 
-  // "de":    deDE,
-  // "de-DE": deDE,
-})
+  ar: arINT,
+  'ar-INT': arINT,
+  de: deDE,
+  'de-DE': deDE,
+  es: esES,
+  'es-ES': esES,
+  fr: frFR,
+  'fr-FR': frFR,
+  it: itIT,
+  'it-IT': itIT,
+  ja: jaJP,
+  'ja-JP': jaJP,
+  nl: nlNL,
+  'nl-NL': nlNL,
+}
